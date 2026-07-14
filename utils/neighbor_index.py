648ec83f94cc8f65b6b2fec9595daf4703eb9fe4
Worpakat <Worpakatkaraman03@gmail.com>
2026-07-14 17:27:53 +0300

    Agent ready to test.

--- utils/neighbor_index.py
+++ utils/neighbor_index.py
@@ -85,8 +85,6 @@ class FaissIndex(NeighborIndex):
 
         k = min(k, len(self.keys))
 
-        print("query shape", query.shape)
-
         _, indices = self.index.search(
             query.detach().cpu().numpy(),
             k,
