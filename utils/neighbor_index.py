803d6afd9aa0484f7565e0488fa53c002b821719
Worpakat <Worpakatkaraman03@gmail.com>
2026-07-15 21:12:14 +0300

    Agent ready to test.

--- utils/neighbor_index.py
+++ utils/neighbor_index.py
@@ -108,14 +108,9 @@ class FaissIndex(NeighborIndex):
 
         index = int(indices[0, 0])
 
-        print(f"query device: {query.device}, keys device: {self.keys.device}")
-
-
         distance = torch.norm(
             self.keys[index] - query.to(self.keys.device)
         )
-
-
 
         if distance <= self.duplicate_threshold:
             return index
