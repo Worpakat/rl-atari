3438c61e3644fc791f2a61e6aecde6cc886e49a6
Worpakat <Worpakatkaraman03@gmail.com>
2026-07-18 18:28:55 +0300

    No KL loss training.

--- models/dnd.py
+++ models/dnd.py
@@ -79,7 +79,7 @@ class DND(nn.Module):
             (self.max_memory, self.representation_dim),
             dtype=torch.float,
             device=self.device,
-            
+            requires_grad=True
         )
         self.values = torch.empty(
             (self.max_memory, self.value_dim),
