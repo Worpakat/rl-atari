9c85f98a2e828b5ae08a992590260c8f1a507c4a
Worpakat <Worpakatkaraman03@gmail.com>
2026-07-20 15:55:13 +0300

    Testing  prioritized replay memory.

--- models/data_buffers.py
+++ models/data_buffers.py
@@ -254,6 +254,9 @@ class ReplayMemory(BaseBuffer):
         td_errors = td_errors.detach().abs().cpu().tolist()
 
         for index, error in zip(indices, td_errors):
+            print("Error:", error)
+            print("Epsilon:", self.priority_epsilon)
+
             self._memory[index].priority = (error + self.priority_epsilon) ** self.priority_alpha 
 
     
