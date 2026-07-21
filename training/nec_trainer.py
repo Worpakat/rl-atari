182bf9f877a822f3595e8e05f91f64dd3c8463ce
Worpakat <Worpakatkaraman03@gmail.com>
2026-07-21 18:27:22 +0300

    Transition removal has been refactored with StaticSequenceHandler.

--- training/nec_trainer.py
+++ training/nec_trainer.py
@@ -82,9 +82,9 @@ class NECTrainer:
         self.sequence_buffer = FrameSequenceBuffer(sequence_length=config.sequence_length)
         self.transition_queue_manager = TransitionQueueManager(capacity=config.transition_queue_size)
         self.static_sequence_handler = RiverRaidStaticSequenceHandler(
-            initial_static_frames=config.static_sequence_handler.initial_static_frames, 
-            intermediate_static_frames=config.static_sequence_handler.death_static_frames,
-            terminal_static_frames=config.static_sequence_handler.terminal_static_frames,
+            initial_static_frames=config["static_sequence_handler"]["initial_static_frames"], 
+            intermediate_static_frames=config["static_sequence_handler"]["death_static_frames"],
+            terminal_static_frames=config["static_sequence_handler"]["terminal_static_frames"],
         )
 
 
