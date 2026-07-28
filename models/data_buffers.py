d72bc5327a02529279b10b930ce40a69f7234621
Worpakat <Worpakatkaraman03@gmail.com>
2026-07-29 00:15:59 +0300

    StratifiedReplayMemory first experiment.

--- models/data_buffers.py
+++ models/data_buffers.py
@@ -410,7 +410,10 @@ class StratifiedReplayMemory():
         # NEW transitions
         # ==========================================================
 
-        new_count = min(network_optimization_period, (self._locked_new_bucket_size - self._new_index))
+        needed_bucket_size = len(self._new_bucket) if self.first_turn else self._locked_new_bucket_size
+
+        new_count = min(network_optimization_period, (needed_bucket_size - self._new_index))
+
 
         for i in range(self._new_index, self._new_index + new_count):
             batch.append(self._new_bucket[i])
