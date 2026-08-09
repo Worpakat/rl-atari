be4f915982262a8cdcaa470b53ea42cb789bff57
Worpakat <Worpakatkaraman03@gmail.com>
2026-08-09 13:23:20 +0300

    long_run_1

--- models/data_buffers.py
+++ models/data_buffers.py
@@ -613,19 +613,13 @@ class StratifiedReplayMemory():
             if transition.bucket == destination_bucket:
                 continue
             
-            print(f"Moving transition from {transition.bucket} to {destination_bucket} | TD-error: {td_error:.4f}")
+            # print(f"Moving transition from {transition.bucket} to {destination_bucket} | TD-error: {td_error:.4f}")
 
             if transition.bucket != ReplayBucketType.WARMUP: # WARMUP transitions are already removed from buckets during sampling.
                 self.buckets[transition.bucket].remove(transition)
 
             transition.bucket = destination_bucket
             self.buckets[destination_bucket].append(transition)
-
-            if transition not in self.buckets[destination_bucket]: 
-            # or not isinstance(transition, ReplayMemoryUnit):
-                # For sanity check, we print the transition and the destination bucket if it is not in the bucket after appending.
-                print(transition)    
-                print(f"Transition not in the bucket {destination_bucket}.")
 
         # Remove oldest transitions if any bucket exceeds its capacity.
         self._clip_buckets()
