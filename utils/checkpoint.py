d842de8c9b13282a28404ae7288239b995a1a5d1
Worpakat <Worpakatkaraman03@gmail.com>
2026-08-19 15:25:23 +0300

    big_replay_memo training continues

--- utils/checkpoint.py
+++ utils/checkpoint.py
@@ -97,7 +97,7 @@ class CheckpointManager:
 
         #THIS ONE IS TEMPORARY, NEED TO BE REMOVED LATER OR REPLACED WITH A BETTER SOLUTION
         if kaggle_execution: # Load from kaggle session local storage
-            local_checkpoint_dir = Path("/kaggle/input/lon-run-0-checkpoint-350")
+            local_checkpoint_dir = Path("/kaggle/input/datasets/worpakat/lon-run-0-checkpoint-350")
             # local_checkpoint_dir = Path("/kaggle/working")
             filepath = local_checkpoint_dir / filename
         else:
